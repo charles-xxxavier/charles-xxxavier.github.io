8be86752ae317fd983f67633950532b9b91bc2e4
charles-xxxavier <150310629+charles-xxxavier@users.noreply.github.com>
2023-11-09 09:45:17 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -10,27 +10,3 @@ myimage.onclick = function () {
     }
 };
 
-//ffffffffffffffffffffffffff
-
-
-var myButton = document.querySelector("button");
-var myHeading = document.querySelector("h1");
-
-function setUserName() {
-    var myName = prompt("Please enter your name.");
-    localStorage.setItem("name", myName);
-    myHeading.textContent = "Здарова, " + myName;
-  }
-
-if (!localStorage.getItem("name")) {
-  setUserName();
-} else {
-  var storedName = localStorage.getItem("name");
-  myHeading.textContent = "Здарова", " + storedName;
-}
-
-
-myButton.onclick = function () {
-    setUserName();
-  };
-  
